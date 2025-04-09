089c5521586913a58805d4114b588b534a73fffa
@REKTBuildr <rektbuildr@crypto.bi>
2025-04-08 21:49:37 -0300

    Added keccak256 hashing class

--- examples/avaxto/wallet/addresses_from_mnemonic.cpp
+++ examples/avaxto/wallet/addresses_from_mnemonic.cpp
@@ -12,13 +12,24 @@
 */
 
 int main(int argc, char const *argv[]){
-    
+
+    int num_addrs = 1;
+
     if (argc < 14) {
         std::cerr << std::format("Usage: %s <number_addrs> < ... words>", argv[0]) << std::endl;
         return 1;
     }
 
-
+    if (argc > 1) {
+        num_addrs = std::stoi(argv[1]);
+    }
+    std::string mnemonic = "";
+    for (int i=2;i<argc;i++) {
+        mnemonic += std::string(argv[i]) + " ";
+    }
+    mnemonic = mnemonic.substr(0, mnemonic.length() - 1); // remove last space
+    std::cout << "Mnemonic: " << mnemonic << std::endl;
+    std::cout << "Number of addresses: " << num_addrs << std::endl;
     
     return 0;
 }
---
+++ include/avaxto/crypto/keccak256.h
@@ -0,0 +1,45 @@
+/* 
+ * Based on:
+ * Bitcoin cryptography library
+ * Copyright (c) Project Nayuki
+ * 
+ * https://www.nayuki.io/page/bitcoin-cryptography-library
+ * https://github.com/nayuki/Bitcoin-Cryptography-Library
+ * 
+ * With modifications
+ * 
+ */
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <bitcoin/system.hpp>
+
+
+/* 
+ * Computes the Keccak-256 hash of a sequence of bytes. The hash value is 32 bytes long.
+ * Provides just one static method.
+ */
+class keccak256 final {
+	
+	public: 
+		static constexpr int HASH_LEN = 32;
+		static void get_hash(const std::uint8_t msg[], std::size_t len, std::uint8_t hashResult[HASH_LEN]);
+		static bc::data_chunk get_hash(const bc::data_chunk& data);
+
+	private: 
+		static constexpr int BLOCK_SIZE = 200 - HASH_LEN * 2;
+		static constexpr int NUM_ROUNDS = 24;
+		static void absorb(std::uint64_t state[5][5]);
+		// Requires 0 <= i <= 63
+		static std::uint64_t rotl64(std::uint64_t x, int i);
+	
+	
+		keccak256() = delete;  // Not instantiable
+	
+	
+	private: 
+		static const unsigned char ROTATION[5][5];
+	
+};
